99bc62a3c0b9fcd46b8ee931a189eaffa87a795a
Vicente Alberola <vicalbyag@alu.edu.gva.es>
2026-01-26 15:28:29 +0100

    Añadida historico a la cuenta destino de una transferencia

--- src/Account/DebitAccount.java
+++ src/Account/DebitAccount.java
@@ -121,7 +121,8 @@ public class DebitAccount extends BankAccount {
                     System.out.println("Operation successful");
                     System.out.println("New balance in " + sourceAcc + " is: " + account.balance);
                     System.out.println("New balance in " + destinationAcc + " is: " + destAcc.balance);
-                    account.getHistory().add(new BankAccountHistory(previousBalance, "Transference", amount, account.balance, transactionDate, destAcc));
+                    account.getHistory().add(new BankAccountHistory(previousBalance, "Transference", -amount, account.balance, transactionDate, destAcc));
+                    destAcc.getHistory().add(new BankAccountHistory(destAcPreviousBalance, "Transference", +amount, destAcc.balance, transactionDate, destAcc));
                 }
                 else{
                     System.out.println("Destination account does not exist");
